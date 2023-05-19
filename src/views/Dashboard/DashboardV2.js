713b69140eec3325261ac061b4698679f265691b
0xLucasd <>
2023-05-19 12:00:53 +0300

    fee dist. date updated

--- src/views/Dashboard/DashboardV2.js
+++ src/views/Dashboard/DashboardV2.js
@@ -403,7 +403,7 @@ export default function DashboardV2() {
                   <div>${formatAmount(totalFees, USD_DECIMALS, 0, true)}</div>
                 </div>
                 <div className="App-card-row padding-left">
-                  <div className="label">Collected fees from May 12, 2023</div>
+                  <div className="label">Collected fees from May 19, 2023</div>
                   <div>${numberWithCommas(totalFeesDistributed.toFixed(0))}</div>
                 </div>
               </div>
